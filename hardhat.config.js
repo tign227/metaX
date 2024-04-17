dbf6b4eca43ac3b1083c1ba1ac9f8597dee41bdc
TingHuang <ting@astriddao.xyz>
2024-04-17 13:12:25 +0800

    feat:hardhat init

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24"
+      },
+      {
+        version: "0.8.20"
+      }
+    ]
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
