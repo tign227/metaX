30232c57092a79c35bc5c034c358e881a8b352bb
MichaelWang <michael_wangguanyi@163.com>
2024-05-09 13:45:32 +0800

    remove event from MetaToken

--- contracts/token/MetaToken.sol
+++ contracts/token/MetaToken.sol
@@ -11,16 +11,11 @@ contract MetaToken is ERC20, Ownable(msg.sender) {
     constructor() ERC20("metaX Token", "xToken") {
     }
 
-    event MetaTokenMinted(address indexed to, uint256 amount);
-    event MetaTokenBurned(address indexed from, uint256 amount);
-
     function mint(address _to, uint256 _amount) onlyOwner public {
         _mint(_to, _amount);
-        emit MetaTokenMinted(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) public {
-        _burn(_from, _amount);
-        emit MetaTokenBurned(_from, _amount);
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,26 +9,25 @@ require("./tasks/metaTokenFaucet")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.24"
-      },
-      {
-        version: "0.8.20"
-      }
-    ]
-  },
-  networks: {
-    hardhat: {
-      forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
-        accounts: [process.env.PRIVATE_KEY]
-      }
-    },
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY]
+    solidity: {
+        compilers: [{
+            version: "0.8.20",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200,
+                },
+            }
+        }]
+    }, networks: {
+        hardhat: {
+            // forking: {
+            //     url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
+            //     accounts: [process.env.PRIVATE_KEY]
+            // }
+        }, sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
+            accounts: [process.env.PRIVATE_KEY]
+        }
     }
-  }
 };
