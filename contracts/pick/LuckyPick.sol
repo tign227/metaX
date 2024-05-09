20765ba5dbc6936632b0209e155d1a34b65da89d
MichaelWang <michael_wangguanyi@163.com>
2024-05-09 13:54:54 +0800

    optimize LuckyPick

--- contracts/pick/LuckyPick.sol
+++ contracts/pick/LuckyPick.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "../raffle/interface/IRaffle.sol";
-import "../token/MetaToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract LuckyPick {
     string public constant NAME = "LuckyPick";
@@ -11,10 +12,10 @@ contract LuckyPick {
     uint256 private ticketCount;
     uint256 private ticketPrice = 100;
     address private operator;
-    mapping( uint256 =>Ticket) private tickets;
+    mapping(uint256 => Ticket) private tickets;
     mapping(address => bool) private hasClaimed;
     uint256 private winningTicketId;
-    MetaToken private xToken;
+    IERC20 private xToken;
     IRaffle private raffle;
     bool private isPicking;
 
@@ -35,9 +36,9 @@ contract LuckyPick {
         _;
     }
 
-    constructor(IRaffle _raffle, address _xToken ) {
+    constructor(IRaffle _raffle, address _xToken) {
         raffle = _raffle;
-        xToken = MetaToken(_xToken);
+        xToken = IERC20(_xToken);
         operator = msg.sender;
     }
 
@@ -47,23 +48,25 @@ contract LuckyPick {
 
     function buyTicket() external {
         require(!isPicking, "LuckyPick: already picking");
+        address sender = msg.sender;
         require(xToken.balanceOf(msg.sender) >= ticketPrice, "ILuckyPick: insufficient balance");
-        xToken.transferFrom(msg.sender, address(this), ticketPrice);
-        tickets[ticketId] = Ticket(ticketId, msg.sender, false);
+        xToken.transferFrom(sender, address(this), ticketPrice);
+        tickets[ticketId] = Ticket(ticketId, sender, false);
         ticketId += 1;
         ticketCount += 1;
-        emit PurchasedTicket(msg.sender, ticketPrice);
+        emit PurchasedTicket(sender, ticketPrice);
     }
 
     function claim() external {
         require(isPicking, "LuckyPick: not picking");
-        require(tickets[winningTicketId].owner == msg.sender, "LuckyPick: not owner");
+        address sender = msg.sender;
+        require(tickets[winningTicketId].owner == sender, "LuckyPick: not owner");
         require(!tickets[ticketId].hasClaimed, "LuckyPick: already claimed");
-        xToken.transfer(msg.sender, ticketPrice);
+        xToken.transfer(sender, ticketPrice);
         tickets[ticketId].hasClaimed = true;
-        emit ClaimReward(msg.sender);
+        emit ClaimReward(sender);
     }
-    
+
     function startPick() external onlyOperator {
         require(!isPicking, "LuckyPick: already picking");
         isPicking = true;
