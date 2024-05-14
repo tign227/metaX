40eee553b71e9e743a74a95c18f5e78921e4158c
TingHuang <ting@astriddao.xyz>
2024-05-14 11:24:34 +0800

    chmod:localhost net

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,6 +32,8 @@ module.exports = {
         }, sepolia: {
             url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}`,
             accounts: [process.env.PRIVATE_KEY]
+        }, localhost: {
+            url: "http://127.0.0.1:8545",
         }
     }
 };
